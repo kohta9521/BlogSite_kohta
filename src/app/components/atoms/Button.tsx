9bc7163b4b510e973c8663194d6b7e3f8faeb209
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-16 21:28:48 +0900

    add button props components methods understand

---
+++ src/app/components/atoms/Button.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import styles from "./Buttom.module.css";
+
+interface ButtonProps {
+  text: string;
+  color?: string;
+  size?: 'small' | "medium" | 'large';
+}
+
+const Button: React.FC<ButtonProps> = ({ text, size = 'medium' }) => {
+
+  // const buttonStyles = {
+  //   backgroundColor: color,
+  // }
+
+  return (
+    <button className={`${styles.button} ${styles[size]}`}>
+      {text}
+    </button>
+  )
+}
+
+export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Button from './components/atoms/Button'
 import styles from './styles/page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       sample code
+      <Button text='Click me!' color='blue' size='large' />
+      <Button text='Hello!' color='green' />
+      <Button text='Pus!' color='red' />
     </main>
   )
 }
