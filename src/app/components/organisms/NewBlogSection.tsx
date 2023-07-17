fcaced6541903513ba2be78f1476b728057a18d6
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-17 21:24:49 +0900

    add new arry about blog section componets #21

--- src/app/components/organisms/NewBlogSection.tsx
+++ src/app/components/organisms/NewBlogSection.tsx
@@ -6,13 +6,53 @@ import Title from "../atoms/Title";
 import BlogCard from "../molecules/BlogCard";
 
 
+// map関数を使用した繰り返し幸運でこの中で配列を作成して
+// それを表示させるのではいけないのか？
+
+// しかしそれだとバックエンドとの接続でいちいち配列内にぶち込む必要、現存のコードだと不可能
+
+
+// とりあえず配列の中に全ての最新記事の内容を保管してみる
+// バックエンドの実装はその後w
+type NewBlogContent = {
+  id: number;
+  date: string;
+  imgUrl: string;
+  titleText: string;
+  explainText: string;
+  tag:string;
+}
+
 const NewBlogSection: React.FC = () => {
+
+// ブログ内容を格納する配列
+// 後々map関数で全てを出力
+  const newBlogContent: NewBlogContent[] = [
+    {
+      id: 1,
+      date: "2021-07-17",
+      imgUrl: "https://placehold.jp/150x150.png",
+      titleText: "ブログタイトル",
+      explainText: "ブログの説明文",
+      tag: "ブログタグ"
+    },
+  ]
+
   return (
     <div className={styles.newBlogBox}>
       <Title id={1} text="最新記事" link="#blog" size="medium"  />
+      {
+        newBlogContent.map((item, i) => (
+          // ここにブログカードを格納、もしくはそれ配下の子コンポーネントを直接記述
+          // 製作中
+          <div className={styles.blogCard}>
+            {item.titleText}
+          </div>
+        ))
+      }
+      {/* <BlogCard />
       <BlogCard />
-      <BlogCard />
-      <BlogCard />
+      <BlogCard /> */}
     </div>
   )
 }
