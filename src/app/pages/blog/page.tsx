305f11510be91cb5daa58356d3270c86d7ca0a1b
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-17 13:43:07 +0900

    add mainlayout header and footer is fixed about mainLayout have children

--- src/app/pages/blog/page.tsx
+++ src/app/pages/blog/page.tsx
@@ -2,12 +2,15 @@ import React, { FC } from "react";
 
 // css import
 import styles from "./BlogPage.module.css";
+import MainLayout from "@/app/components/templates/MainLayout";
 
 
 const BlogPage: React.FC = () => {
   return (
     <>
-    Blog Page!
+      <MainLayout>
+        <h1>Blog page!</h1>
+      </MainLayout>
     </>
   )
 }
