76db0dd0ae955fbfa83c2fe70e5410ad808627cf
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-17 15:03:15 +0900

    add heroTitle components interface type anotation

--- src/app/components/atoms/HeroTitle.tsx
+++ src/app/components/atoms/HeroTitle.tsx
@@ -7,14 +7,20 @@ import styles from "./HeroTitle.module.css";
 interface HeroProps {
   id: number;
   title: string;
+  link: string;
   subTitle: string;
 }
 
 
-const HeroTitle: React.FC = () => {
+const HeroTitle: React.FC<HeroProps> = ({ id, title, link,  subTitle }) => {
   return (
     <div className={styles.heroTitleBox}>
-      hero title
+      <a key={id} className={styles.title} href={link}>
+        {title}
+      </a>
+      <a key={id} className={styles.subtitle} href={link}>
+        {subTitle}
+      </a>
     </div>
   )
 }
--- src/app/components/organisms/Hero.tsx
+++ src/app/components/organisms/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: React.FC = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.container}>
-        <HeroTitle />
+        <HeroTitle id={1} title="Next.jsにおけるcssのベストプラクティス" subTitle="cssの選定と理由" link="/pages/blog" />
       </div>
     </div>
   )
