9f76af4a3a7accbee884fcc378781c446b2e806d
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-17 13:11:24 +0900

    add all menuList touring page and sublayout atomic design templates components

--- src/app/components/molecules/ListArea.tsx
+++ src/app/components/molecules/ListArea.tsx
@@ -9,7 +9,10 @@ const ListArea: React.FC = () => {
   return (
     <div className={styles.listArea}>
       <MenuList id={1} text="Home" link="/" size="medium"  />
-      <MenuList id={2} text="Blog" link="/pages/blog" size="medium"  />
+      <MenuList id={2} text="About" link="/pages/about" size="medium"  />
+      <MenuList id={3} text="Blog" link="/pages/blog" size="medium"  />
+      <MenuList id={4} text="Work" link="/pages/work" size="medium"  />
+      <MenuList id={5} text="Contact" link="/pages/contact" size="medium"  />
     </div>
   )
 }
--- src/app/pages/blog/page.tsx
+++ src/app/pages/blog/page.tsx
@@ -1,5 +1,15 @@
-function Product() {
-  return <h2>Product Page</h2>
- }
- 
- export default Product;
+import React, { FC } from "react";
+
+// css import
+import styles from "./BlogPage.module.css";
+
+
+const BlogPage: React.FC = () => {
+  return (
+    <>
+    Blog Page!
+    </>
+  )
+}
+
+export default BlogPage;
