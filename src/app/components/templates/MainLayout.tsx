a399ecf278b20503d9ffecb29ed8d392b30a7706
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-04 08:14:56 +0900

    add new atomic layout

---
+++ src/app/components/templates/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/Mainlayout.module.css";
+
+
+const MainLayout: React.FC = () => {
+  return (
+    <>
+    <p>main layout</p>
+    </>
+  )
+}
+
+export default MainLayout;
---
+++ src/app/pages/blog/page.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./BlogPage.module.css";
+
+
+const BlogPage: React.FC = () => {
+  return (
+    <>
+        <h1>Blog page!</h1>
+    </>
+  )
+}
+
+export default BlogPage;
+
+// /pages/blogのリンク
