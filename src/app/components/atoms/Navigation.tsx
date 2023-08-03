ce43b02e22527d83cabda913dbeefeed44ffdd96
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-04 08:54:02 +0900

    add header button

---
+++ src/app/components/atoms/Navigation.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/Navigation.module.css";
+
+
+type Props = {
+  open: boolean;
+  id: string;
+};
+
+
+const Navigation: React.FC<Props> = ({ open, id }) => {
+  return (
+    <nav id={id} aria-hidden={!open} className={styles.navigation} >
+      <ul>
+        <li>about</li>
+        <li>about</li>
+        <li>about</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
