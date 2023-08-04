94f735fd6b9090f08d356937356ebae161ce5d82
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-04 21:24:12 +0900

    add topcard pc css

--- src/app/components/molecules/TopBlogCard.tsx
+++ src/app/components/molecules/TopBlogCard.tsx
@@ -6,6 +6,7 @@ import styles from "./styles/TopBlogCard.module.css";
 // props type
 type Props = {
   id: number;
+  blogLink: string;
   imgUrl: string;
   title: string;
   date: string;
@@ -13,26 +14,28 @@ type Props = {
 }
 
 
-const TopBlogCard: React.FC<Props> = ({ id, imgUrl, title, date, text }) => {
+const TopBlogCard: React.FC<Props> = ({ id, blogLink, imgUrl, title, date, text }) => {
   return (
-    <div className={styles.topCard} key={id}>
-      <div className={styles.imgBox}>
-        <img className={styles.img} src={imgUrl} alt="topCardImg" />
-      </div>
-      <div className={styles.textBox}>
-        <h2 className={styles.title}>
-          {title}
-        </h2>
-        <p className={styles.date}>
-          {date}
-        </p>
-        <p className={styles.text}>
-          {text}
-        </p>
-        <div className={styles.tagsBox}>
-          tags
+    <div className={styles.topCard} key={id} >
+      <a className={styles.blogLink} href={blogLink}>
+        <div className={styles.imgBox}>
+          <img className={styles.img} src={imgUrl} alt="topCardImg" />
         </div>
-      </div>
+        <div className={styles.textBox}>
+          <h2 className={styles.title}>
+            {title}
+          </h2>
+          <p className={styles.date}>
+            {date}
+          </p>
+          <p className={styles.text}>
+            {text}
+          </p>
+          <div className={styles.tagsBox}>
+            tags
+          </div>
+        </div>
+      </a>
     </div>
   )
 }
