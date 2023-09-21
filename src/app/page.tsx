b0d77aa7c6aa39e36ea2029636822c52bfa7da40
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-21 21:01:56 +0900

    remove not sue files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 import styles from '../styles/page.module.css'
-import Page from './pages'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* pagesディレクトリ内のファイル　ルーティング設定のため */}
-      <Page />
+<h1>hello</h1>
     </main>
   )
 }
