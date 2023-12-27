f743ef30e601ce6191e608a565f8445a63696b99
k_95.21 <kohta0421kochi@gmail.com>
2023-12-27 11:49:55 +0900

    add develop

---
+++ src/components/organisms/LeftSidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+// scss
+import styles from './styles/LeftSidebar.module.scss'
+
+
+const LeftSidebar = () => {
+    return (
+        <div>
+            LeftSidebar
+        </div>
+    )
+}
+
+export default LeftSidebar
