0b08eb23369104c24dd876e19eb0d011f154a77b
k_95.21 <kohta0421kochi@gmail.com>
2023-12-27 12:41:51 +0900

    add new hamburgermenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import BlogPost from '../components/molecules/BlogPost';
+import Header from '@/components/organisms/Header';
+
 
 export default function Home() {
 
   return (
     <div>
-      <BlogPost postId="hNSlTwwb0YBFJ1REvZnm" />
-      <BlogPost postId="MPs2HlCIhSnye0W1Pjke" />
-      <BlogPost postId="glulgi8fhpxWTJ8cK3J2" />
+      <Header />
     </div>
   )
 }
---
+++ src/components/molecules/Nav.tsx
@@ -0,0 +1,67 @@
+'use client';
+import React, { useState } from 'react'
+import styles from './styles/Nav.module.scss';
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { menuSlide } from '../../animation/anim';
+import Link from 'next/link';
+
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+]
+
+export default function index() {
+
+  const pathname = usePathname();
+  const [selectedIndicator, setSelectedIndicator] = useState(pathname);
+
+  return (
+    <motion.div 
+      variants={menuSlide} 
+      initial="initial" 
+      animate="enter" 
+      exit="exit" 
+      className={styles.menu}
+      >
+       <div className={styles.body}>
+            <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
+                    <div className={styles.header}>
+                        <p>Navigation</p>
+                    </div>
+                    {
+                      navItems.map( (data, index) => {
+                        return <Link
+                        key={index}
+                        data={{...data, index}}
+                        href={data.href}
+                        isActive={selectedIndicator == data.href}
+                        setSelectedIndicator={setSelectedIndicator}>
+                        </Link>
+                      })
+                    }
+            </div>
+            <div className={styles.footer}>
+                <a>Awwwards</a>
+                <a>Instagram</a>
+                <a>Dribble</a>
+                <a>LinkedIn</a>
+            </div>
+        </div>
+    </motion.div>
+  )
+}
