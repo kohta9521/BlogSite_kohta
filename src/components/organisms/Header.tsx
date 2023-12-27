2104115bec87dc03c87aed213c283feda9c3ba52
k_95.21 <kohta0421kochi@gmail.com>
2023-12-27 17:17:33 +0900

    delete all header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,26 +1,16 @@
-'use client'
-import React, { useState } from 'react'
+import React from 'react'
 
 // scss
-import styles from './styles/Header.module.scss'
-import { AnimatePresence } from 'framer-motion';
 
 // components
-import Nav from '../molecules/Nav';
+
 
 const Header = () => {
-    const [ isActive, setIsActive ] = useState(false);
 
     return (
-        <>
-            <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
-                <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}>
-                </div>
-            </div>
-            <AnimatePresence mode='wait'>
-                {isActive && <Nav />}
-            </AnimatePresence>
-        </>
+        <header>
+            header
+        </header>
     )
 }
 
