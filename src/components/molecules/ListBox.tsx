fd285de530a412c252f035c51d7420fae59350ce
k_95.21 <kohta0421kochi@gmail.com>
2023-12-29 09:12:19 +0900

    add moreboxbutton

--- src/components/molecules/ListBox.tsx
+++ src/components/molecules/ListBox.tsx
@@ -5,6 +5,7 @@ import styles from './styles/ListBox.module.scss'
 
 // componets
 import List from '../atoms/List'
+import BoxButton from '../atoms/BoxButton'
 
 const ListBox = () => {
     return (
@@ -19,23 +20,30 @@ const ListBox = () => {
             <List
                 id={1}
                 text='Kohta Blogとは'
-                link='/'
+                link='/about'
                 size='medium'
                 color='black'
             />
             <List
                 id={1}
                 text='記事一覧'
-                link='/'
+                link='/posts'
                 size='medium'
                 color='black'
             />
             <List
                 id={1}
                 text='ジャンル一覧'
-                link='/'
+                link='/genre'
                 size='medium'
                 color='black'
+            />
+            <BoxButton
+                id={1}
+                text='会員登録'
+                size='medium'
+                color='blue'
+                link='/login'
             />
         </div>
     )
