a818cde902827c3d9cd46184083f48ae1a9746c9
k_95.21 <kohta0421kochi@gmail.com>
2024-04-15 22:58:49 +0900

    add new blgo setup

---
+++ frontend/src/components/organisms/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// scss
+import styles from './styles/Header.module.scss'
+
+// components
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}></div>
+    </header>
+  )
+}
+
+export default Header
