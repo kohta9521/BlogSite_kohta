164216a4a9536aabb872cbad83b0652338071cbc
k_95.21 <kohta0421kochi@gmail.com>
2024-04-18 16:01:11 +0900

    add new error apge about 404 and 500 error page 404page complete

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// scss
+import styles from './error/Not-fontd.module.scss'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>hello this page is 404 error!!</h1>
+    </div>
+  )
+}
+
+export default NotFound
