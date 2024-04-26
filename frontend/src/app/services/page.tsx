d9469dd14f31f789c42311503bd94925169df4f1
k_95.21 <kohta0421kochi@gmail.com>
2024-04-26 15:51:25 +0900

    add new page about serviecs page

---
+++ frontend/src/app/services/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// scss
+import styles from './Services.module.scss'
+
+// components
+
+const Services = () => {
+  return (
+    <div className={styles.service}>
+      <h1>this is service page</h1>
+    </div>
+  )
+}
+
+export default Services
