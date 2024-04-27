9620f8bf8f5681204f74b0df47e3bb58cd6ff259
k_95.21 <kohta0421kochi@gmail.com>
2024-04-27 17:43:55 +0900

    add new menu child components

--- frontend/src/components/organisms/Menu.tsx
+++ frontend/src/components/organisms/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // scss
 import styles from './styles/Menu.module.scss'
+import MenuMainList from '../atoms/MenuMainList'
 
 // components
 
@@ -12,7 +13,16 @@ interface MenuProps {
 const Menu = ({ isOpen }: MenuProps) => {
   return (
     <div className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.leftBox}>
+          <h2 className={styles.menuTitle}>MainMenu</h2>
+          <MenuMainList id={1} link='#about' text='About Me' />
+        </div>
+        <div className={styles.centerBox}>
+          <h2 className={styles.menuTitle}>PageMenu</h2>
+        </div>
+        <div className={styles.rightBox}></div>
+      </div>
     </div>
   )
 }
