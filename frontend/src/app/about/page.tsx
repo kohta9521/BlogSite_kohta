940947c61122b17bd0d1e8672159199d0fbeb33f
k_95.21 <kohta0421kochi@gmail.com>
2024-05-13 10:17:28 +0900

    add new subapge section tile and new page layouts

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,6 +2,9 @@
 // import styles from './About.module.scss'
 
 // components
+import SubMainText from '@/components/atoms/SubMainText'
+import SubPageSecTitle from '@/components/atoms/SubPageSecTitle'
+import SubPageTitle from '@/components/atoms/SubPageTitle'
 import SubPageLayout from '@/components/templates/SubPageLayout'
 
 const About = () => {
@@ -12,10 +15,15 @@ const About = () => {
       jaTitle='# このサイトについて'
       desc='このサイトは、Next.jsで作成したサンプルサイトです。'
     >
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, ex nihil vel reprehenderit
-        quam ipsa officia dignissimos magnam nemo quaerat.
-      </p>
+      <SubPageSecTitle id={1} title='What is this site?' />
+      <SubPageTitle id={1} enTitle='About' jaTitle='このサイトについて' />
+      <SubMainText id={1} text='このサイトは、Next.jsで作成したサンプルサイトです。' />
+      <SubMainText
+        id={2}
+        text='このブログは、私の学んだ知識と経験を共有するためのプラットフォームです。ここでは、ウェブ開発、プログラミング、テクノロジーのトレンドなど、私が情熱を持って'
+      />
+      <SubMainText id={3} text='このサイトは、Next.jsで作成したサンプルサイトです。' />
+      <SubMainText id={4} text='このサイトは、Next.jsで作成したサンプルサイトです。' />
     </SubPageLayout>
   )
 }
