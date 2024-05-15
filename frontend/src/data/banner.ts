b5ccd02c3d9cdaea8dc8cd3761d1357eb3d32d6b
k_95.21 <kohta0421kochi@gmail.com>
2024-05-15 17:47:23 +0900

    add new banner responsive design

--- frontend/src/data/banner.ts
+++ frontend/src/data/banner.ts
@@ -1,20 +1,20 @@
 export const bannerHTML = [
   {
     html: `
-    <a href="https://px.a8.net/svt/ejp?a8mat=3Z72YI+2WSB8Y+2PEO+1HQDCH" rel="nofollow">
-    <img border="0" width="468" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=240515514176&wid=001&eno=01&mid=s00000012624009025000&mc=1"></a>
+    <a className={styles.banner} href="https://px.a8.net/svt/ejp?a8mat=3Z72YI+2WSB8Y+2PEO+1HQDCH" rel="nofollow">
+    <img border="0" width="400" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=240515514176&wid=001&eno=01&mid=s00000012624009025000&mc=1"></a>
     <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z72YI+2WSB8Y+2PEO+1HQDCH" alt="">`,
   },
   {
     html: `
-    <a href="https://px.a8.net/svt/ejp?a8mat=3Z72YI+2WSB8Y+2PEO+1HZSUP" rel="nofollow">
-<img border="0" width="468" height="60" alt="" src="https://www25.a8.net/svt/bgt?aid=240515514176&wid=001&eno=01&mid=s00000012624009069000&mc=1"></a>
+    <a className={styles.banner}  href="https://px.a8.net/svt/ejp?a8mat=3Z72YI+2WSB8Y+2PEO+1HZSUP" rel="nofollow">
+<img border="0" width="400" height="60" alt="" src="https://www25.a8.net/svt/bgt?aid=240515514176&wid=001&eno=01&mid=s00000012624009069000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3Z72YI+2WSB8Y+2PEO+1HZSUP" alt="">`,
   },
   {
     html: `
-    <a href="https://px.a8.net/svt/ejp?a8mat=3Z72YG+B5VM42+0K+106APT" rel="nofollow">
-    <img border="0" width="448" height="66" alt="" src="https://www25.a8.net/svt/bgt?aid=240515512675&wid=001&eno=01&mid=s00000000002006076000&mc=1"></a>
+    <a className={styles.banner}  href="https://px.a8.net/svt/ejp?a8mat=3Z72YG+B5VM42+0K+106APT" rel="nofollow">
+    <img border="0" width="400" height="66" alt="" src="https://www25.a8.net/svt/bgt?aid=240515512675&wid=001&eno=01&mid=s00000000002006076000&mc=1"></a>
     <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3Z72YG+B5VM42+0K+106APT" alt="">`,
   },
 ]
